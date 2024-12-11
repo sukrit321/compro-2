6d99e2c2e44478e41de279f748ef099477cb8af5
sukrit321 <54735307+sukrit321@users.noreply.github.com>
2024-12-11 12:09:52 +0700

    Add files via upload

--- indexOf2.java
+++ indexOf2.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class indexOf2 {
     public static void main(String[] args) {
         Scanner sc = new  Scanner(System.in);
@@ -7,28 +6,10 @@ public class indexOf2 {
         String a = sc.nextLine();
         int num = 0;
         int index = 0;
-        /*for(;;){
-            int z = n.indexOf(a, index);
-            if(z == -1){
-                break;
-            }
-            else {
-                index = z + 1;
-                System.out.print(z + 1 + " ");
-                num++;
-
-            }
-        }
-        if(num == 0){
-            System.out.println("string not found");
-        }*/
         for(int i = n.indexOf(a); i != -1; i=n.indexOf(a, i)){
-            System.out.println(i + 1);
+            System.out.print(i + 1 + " ");
             i+=1;
             num++;
-
-
-
         }
         if(num == 0){
             System.out.println("string not found");
